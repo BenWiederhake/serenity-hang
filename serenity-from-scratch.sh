a0c1b958383e9289f2017b92e4f37adccba5861b
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2020-07-25 03:07:49 +0200

    Proper logging, remove script-debugging

--- serenity-from-scratch.sh
+++ serenity-from-scratch.sh
@@ -8,19 +8,17 @@ rm -rf serenity
 date
 git clone https://github.com/SerenityOS/serenity.git
 date
-false
+cd serenity
 date
-#cd serenity
-#date
-#./Toolchain/BuildIt.sh
-#date
-#cd Build
-#date
-#make
-#date
-#make install
-#date
-#make image
-#date
-#make run
-} | tee full-build-from-scratch.log
+./Toolchain/BuildIt.sh
+date
+cd Build
+date
+make
+date
+make install
+date
+make image
+date
+make run
+} 2>&1 | tee full-build-from-scratch-$(date +%s).log
